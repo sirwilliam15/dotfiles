c069e1d10bd98839e59e02529d50bf41a64084b7
sirwilliam15 <sirwilliam15@github.com>
2025-05-12 09:44:27 -0700

    Added common bash commands

---
+++ rc_common.sh
@@ -0,0 +1,32 @@
+
+# Source .env file if it exists
+if [[ -f "$HOME/.env" ]]; then
+    set -a  # automatically export all variables
+    source "$HOME/.env"
+    set +a  # stop automatically exporting
+fi
+
+# alias commands
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias lasc='ls -lahSr'
+alias ldesc='ls -lahS'
+
+alias duh='du -h --max-depth=1'
+
+alias clone="$HOME/.dotfiles/scripts/configure-repo.sh"
+alias rebase="$HOME/.dotfiles/scripts/rebase.sh"
+
+# Ceph Docker function
+ceph-cli() {
+    local version=${1:-v18.2.4}
+    docker run -it --rm \
+        -v /etc/ceph:/etc/ceph \
+        -v /tmp/ceph:/tmp/ceph \
+        -v /var/lib/ceph:/var/lib/ceph \
+        -v /var/log/ceph:/var/log/ceph \
+        quay.io/ceph/ceph:${version} \
+        /bin/bash
+}

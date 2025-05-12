00bebc8154c12506732a532532a149268599b818
sirwilliam15 <sirwilliam15@github.com>
2025-05-12 10:01:03 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 
+mkdir -p "$HOME/.dotfiles/backup"
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Loop through specific config files
     for config in ".yabairc" ".skhdrc" ".bashrc"; do
         if [[ -f "$HOME/$config" ]]; then
             echo "Found $config"
+            if [[ "$config" == ".bashrc" ]]; then
+                echo "Adding source to $HOME/$config"
+                echo "source $HOME/.dotfiles/macos/.bashrc" >> "$HOME/$config"
+                continue
+            fi
             # Add your commands here to handle each config file
             echo "Would you like to overwrite $config? (y/n)"
             read -r overwrite
             if [[ "$overwrite" == "y" ]]; then
+                echo "Backing up $HOME/$config to $HOME/.dotfiles/backup/$config"
+                mv "$HOME/$config" "$HOME/.dotfiles/backup/$config"
+                echo "Creating symlink for $config"
                 ln -sf "$HOME/.dotfiles/macos/$config" "$HOME/$config"
                 echo "Overwritten $config"
             else
@@ -19,3 +29,5 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
         fi
     done
 fi
+
+echo "Installation complete"
